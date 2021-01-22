b70f14d36542f62ca9050e0f94235f17bef48e00
Mehdi Abdennour <mehdi.abdennour@outlook.com>
2021-01-22 12:49:59 +0100

    commit 2

--- main.c
+++ main.c
@@ -5,7 +5,24 @@
 
 int main()
 {
-	printf("hello, world");
+	int file_size;
+	int *fsize = &file_size;
+
+	int *buffer;
+
+	FILE *original;
+	FILE *copie;
+
+
+	original = fopen("sample/attention.wav", "rb");
+	copie = fopen("result/copie_attention.wav", "wb");
+
+	buffer = (int*)malloc(lseek(original, 0L, SEEK_END));
+
+	fread(buffer, lseek(original, 0L, SEEK_END), 1, original);
+	fwrite(buffer, lseek(original, 0L, SEEK_END),1, copie);
+
+	//printf("%d\n", file_size);
 
 	return 0;
 }
