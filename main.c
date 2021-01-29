9b2492293a586e15eacd1ffa47c25ab7e176df3e
Mehdi Abdennour <mehdi.abdennour@outlook.com>
2021-01-29 10:12:42 +0100

    Prototype et fonction Copie

--- main.c
+++ main.c
@@ -1,38 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-//#include "main.h"
+#include "main.h"
+
+void copie(FILE* original, int file_size);
 
 int main()
 {
+		// Declaration des variables et pointeurs
+
 	int file_size;
-	int *fsize = &file_size;
 
-	int *buffer;
+	int* buffer;
 
-//	printf("0");
+	FILE* original;
+//	FILE* copie;
 
-	FILE *original;
-	FILE *copie;
 
-	original = fopen("sample/attention.wav", "rb"); 
-//	printf("1");
-	copie = fopen("result/copie_attention.wav", "wb");
-//	printf("2");
+		// Ouverture des fichiers
+
+	original = fopen("sample/attention.wav", "rb");
+//	copie = fopen("result/copie_attention.wav", "wb");
+
+
+		// Taille du fichier à copier
 
 	fseek(original, 0, SEEK_END);
 	file_size = ftell(original);
 	fseek(original, 0, SEEK_SET);
 
 
-	buffer = (int*)malloc(file_size);
-//	printf("3");
+		// Copie
 
-	fread(buffer, file_size, 1, original);
-//	printf("4");
-	fwrite(buffer, file_size, 1, copie);
-//	printf("5");
-	//printf("%d\n", file_size);
+	copie(original, file_size);
 
+//	buffer = (int*)malloc(file_size);
+
+//	fread(buffer, file_size, 1, original);
+
+//	fwrite(buffer, file_size, 1, copie);
+
+
+		//Fermeture des fichiers
+
+	fclose(original);
+//	fclose(copie);
 	return 0;
 }
+
+void copie(FILE* original, int file_size)
+{
+	int* buffer;
+
+        FILE* copie;
+
+
+                // Ouverture des fichiers
+
+        copie = fopen("result/copie_attention.wav", "wb");
+
+
+                // Copie
+
+        buffer = (int*)malloc(file_size);
+
+        fread(buffer, file_size, 1, original);
+
+        fwrite(buffer, file_size, 1, copie);
+
+
+                //Fermeture des fichiers
+
+        fclose(copie);
+
+}
+
+
