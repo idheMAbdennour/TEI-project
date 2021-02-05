17b06f769df7365588e5d3f38ffceeb263279f0b
Mehdi Abdennour <mehdi.abdennour@outlook.com>
2021-02-05 10:26:30 +0100

    Intégration effective des fonction de dilatation et de compression 	- Encodage des .raw en unsigned 16 bit PCR via int

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+//#include <stdint.h>
 #include <math.h>
 #include "main.h"
 
 void copie(FILE* original, int file_size);
 void dilater(FILE* original, int file_size);
+void compresser(FILE* original, int file_size);
 
 int main()
 {
@@ -27,14 +29,17 @@ int main()
 	fseek(original, 0, SEEK_SET);
 
 
-		// Copie
+		// Copier
 
-//	copie(original, file_size);
+	copie(original, file_size);
 
 		// Dilater
 
 	dilater(original, file_size);
-printf("4 ");
+
+		// Compresser
+
+	compresser(original, file_size);
 
 		//Fermeture des fichiers
 
@@ -45,7 +50,7 @@ printf("4 ");
 
 void copie(FILE* original, int file_size)
 {
-	int* buffer;
+	int16_t* buffer;
 
         FILE* copie;
 
@@ -57,7 +62,7 @@ void copie(FILE* original, int file_size)
 
                 // Copie
 
-        buffer = (int*)malloc(file_size);
+        buffer = (int16_t*)malloc(file_size);
 
         fread(buffer, file_size, 1, original);
 
@@ -65,6 +70,8 @@ void copie(FILE* original, int file_size)
 
 
                 //Fermeture des fichiers
+
+	free(buffer);
 
         fclose(copie);
 
@@ -74,48 +81,127 @@ void dilater(FILE* original, int file_size)
 {
 	int new_file_size = 2 * (file_size - 44);
 
-	char* buffer_old;
-	char* buffer_new;
+	int* buffer_old;
+	int* buffer_new;
 
         FILE* res;
-printf("0000 ");
 
 	fseek(original, 44, SEEK_SET);
 
+
                 // Ouverture des fichiers
-printf("000 ");
+
         res = fopen("result/dilatation_attention.raw", "wb");
 
 
                 // Mise en tampon de l'original
-printf("00 ");
-        buffer_old = (char*)malloc(file_size - 44);
-printf("0 ");
+
+        buffer_old = (int*)malloc((file_size - 44)*sizeof(int));
+
 
         fread(buffer_old, file_size - 44, 1, original);
-printf("1 ");
+
 
 
 		// Duplication de chaque valeur
 
-	buffer_new = (char*)malloc(new_file_size);
-printf("2 ");
+	buffer_new = (int*)malloc(new_file_size*sizeof(int));
 
-	for(int i = 0, j = 0; i < file_size - 44; i++)
+	for(int i=0, j=0; i < file_size-44; i++, j++)
 	{
 		buffer_new[j] = buffer_old[i];
-			j=j+1;
-		buffer_new[j] = buffer_old[i];
-			j=j+1;
+
+ 		buffer_new[++j] = buffer_old[i];
+		//printf("|%d %d", i, j);
 	}
-printf("3 ");
+
+/*	for(int i=0; i<40; i++)
+	{
+		printf("%d ", buffer_old[i]);
+	}
+
+	printf("\n");
+
+	for(int i=0; i<40; i++)
+	{
+		printf("%d ", buffer_new[i]);
+	}
+	printf("\n");
+*/
         fwrite(buffer_new, new_file_size, 1, res);
-printf("4 ");
 
                 //Fermeture des fichiers
+
+	free(buffer_old);
+
+	free(buffer_new);
 
         fclose(res);
 
 }
+
+
+void compresser(FILE* original, int file_size)
+{
+	int new_file_size = (file_size - 44)/2;
+
+	int16_t* buffer_old;
+	int16_t* buffer_new;
+
+        FILE* res;
+
+	fseek(original, 44, SEEK_SET);
+
+
+                // Ouverture des fichiers
+
+        res = fopen("result/compression_attention.raw", "wb");
+
+
+                // Mise en tampon de l'original
+
+        buffer_old = (int16_t*)malloc((file_size-44)*sizeof(int16_t));
+
+
+        fread(buffer_old, file_size - 44, 1, original);
+
+
+
+		// Duplication de chaque valeur
+
+	buffer_new = (int16_t*)malloc(new_file_size*sizeof(int16_t));
+
+	for(int i=0, j=0; (i < new_file_size)&&(j < file_size-44) ; i++, j+=2)
+	{
+		buffer_new[i] = buffer_old[j];
+		//printf("|%d %d", i, j);
+	}
+
+/*	for(int i=0; i<40; i++)
+	{
+		printf("%d ", buffer_old[i]);
+	}
+
+	printf("\n");
+
+	for(int i=0; i<40; i++)
+	{
+		printf("%d ", buffer_new[i]);
+	}
+	printf("\n");
+*/
+        fwrite(buffer_new, new_file_size, 1, res);
+
+
+                //Fermeture des fichiers
+
+	free(buffer_old);
+
+	free(buffer_new);
+
+        fclose(res);
+
+}
+
 
 
