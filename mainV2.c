979fed91678d256fd41aa8af42051bbc113cbe89
Mehdi Abdennour <mehdi.abdennour@outlook.com>
2021-03-19 12:38:59 +0100

    Dernière modification : changer les cast dans les deux autres fonctions grave

--- mainV2.c
+++ mainV2.c
@@ -11,19 +11,19 @@ void slice();
 
 typedef struct
 {
-    unsigned long FileTypeBlocID;
-    unsigned long FileSize;
-    unsigned long FileFormatID;
-    unsigned long FormatBlocID;
-    unsigned long BlocSize;
+    uint8_t FileTypeBlocID[4];
+    uint32_t FileSize;
+    uint8_t FileFormatID[4];
+    uint8_t FormatBlocID[4];
+    uint32_t BlocSize;
     uint16_t AudioFormat;
     uint16_t NbrCanaux;
-    unsigned long Frequence;
-    unsigned long BytePerSec;
+    uint32_t Frequence;
+    uint32_t BytePerSec;
     uint16_t BytePerBloc;
     uint16_t BitsPerSample;
-    unsigned long DataBlocID;
-    unsigned long DataSize;
+    uint8_t DataBlocID[4];
+    uint32_t DataSize;
   
 }wav_h;
 
@@ -33,7 +33,7 @@ typedef struct
 
     wav_h Header;    
 
-    char* Data;
+    uint8_t* Data;
     
 }fichier_wav;
 
@@ -44,6 +44,22 @@ fichier_wav source;
 int main()
 {
     file_handle();
+
+            printf("FileTypeBlocID : %s\n", source.Header.FileTypeBlocID);
+            printf("FileSize : %u \n", source.Header.FileSize);
+            printf("FileFormatID : %s\n", source.Header.FileFormatID);
+            printf("FormatBlocID : %s\n", source.Header.FormatBlocID);
+            printf("BlocSize : %u\n", source.Header.BlocSize);
+            printf("AudioFormat : %u\n", source.Header.AudioFormat);
+            printf("NbrCanaux : %u\n", source.Header.NbrCanaux);
+            printf("Frequence : %u\n", source.Header.Frequence);
+            printf("BytePerSec : %u\n", source.Header.BytePerSec);
+            printf("BytePerBloc : %u\n", source.Header.BytePerBloc);
+            printf("BitsPerSample : %u\n", source.Header.BitsPerSample);
+            printf("DataBlocID : %s\n", source.Header.DataBlocID);
+            printf("DataSize : %u\n", source.Header.DataSize);
+
+
     
     copie(source.File);
 
@@ -56,7 +72,7 @@ void copie()
 {   
     fichier_wav copie; 
     
-    unsigned long file_size = source.Header.FileSize;
+    uint32_t file_size = source.Header.FileSize;
 
 
                 // Ouverture des fichiers
@@ -66,25 +82,166 @@ void copie()
 
                 // Copie
 
-    buffer = (int16_t*)malloc(file_size);
+    int16_t* buffer = (int16_t*)malloc(file_size);
 
-    fread(buffer, file_size, 1, original);
+    fread(buffer, file_size, 1, source.File);
 
-    fwrite(buffer, file_size, 1, copie);
+    fwrite(buffer, file_size, 1, copie.File);
 
 
                 //Fermeture des fichiers
 
 	free(buffer);
 
-    fclose(copie);	
+    fclose(copie.File);	
 
 }
 
-void grave()
-{
-	fichier_wav grave;
+void grave8()
+{        
+            // Déclaration et initialisation de la structure grave
+    
+    fichier_wav grave;
+    grave.File = fopen("result/grave.wav", "wb");
+    grave.Header = source.Header;
+    grave.Data = (uint8_t*)malloc(grave.Header.DataSize);    
+
+
+    		// Duplication de chaque valeur
    
+	uint8_t* buffer = (uint8_t*)malloc(grave.Header.DataSize * 2);
+
+	for(int i=0, j=0; i < grave.Header.DataSize; i++, j++)
+	{
+		buffer[j] = source.Data[i];
+
+ 		buffer[++j] = source.Data[i];
+	}
+
+
+            // Mise à l'échelle  
+    
+    int k=0;
+
+	int p = 1;		                                // durée d'un segment en ms
+
+	int seg = grave.Header.Frequence*p/1000;	    //nombre d'échantillon dans un segment de durée p  
+
+	for(int i=0; i < grave.Header.Datasize - seg; i=i+seg)
+	{
+		for(int j=0; j < seg; j++)
+		{
+			grave.Data[j+i] = (buffer[j+i+seg+k*(seg-1)] + buffer[j+i+seg+k*(seg-1)])/2;
+		}
+		k++;
+
+	}  
+
+    fwrite(grave.Data, grave.Header.Datasize, 1, grave.File);
+
+	free(buffer);
+    free(grave.Data);
+
+	fclose(grave.File);
+
+}
+
+void grave16()
+{        
+            // Déclaration et initialisation de la structure grave
+    
+    fichier_wav grave;
+    grave.File = fopen("result/grave.wav", "wb");
+    grave.Header = source.Header;
+    grave.Data = (uint8_t*)malloc(grave.Header.DataSize);    
+
+
+    		// Duplication de chaque valeur
+   
+	uint8_t* buffer = (uint8_t*)malloc(grave.Header.DataSize * 2);
+
+	for(int i=0, j=0; i < grave.Header.DataSize; i++, j++)
+	{
+		buffer[j] = source.Data[i];
+
+ 		buffer[++j] = source.Data[i];
+	}
+
+
+            // Mise à l'échelle  
+    
+    int k=0;
+
+	int p = 1;		                                // durée d'un segment en ms
+
+	int seg = grave.Header.Frequence*p/1000;	    //nombre d'échantillon dans un segment de durée p  
+
+	for(int i=0; i < grave.Header.Datasize - seg; i=i+seg)
+	{
+		for(int j=0; j < seg; j++)
+		{
+			grave.Data[j+i] = (buffer[j+i+seg+k*(seg-1)] + buffer[j+i+seg+k*(seg-1)])/2;
+		}
+		k++;
+
+	}  
+
+    fwrite(grave.Data, grave.Header.Datasize, 1, grave.File);
+
+	free(buffer);
+    free(grave.Data);
+
+	fclose(grave.File);
+
+}
+
+void grave32()
+{        
+            // Déclaration et initialisation de la structure grave
+    
+    fichier_wav grave;
+    grave.File = fopen("result/grave.wav", "wb");
+    grave.Header = source.Header;
+    grave.Data = (uint8_t*)malloc(grave.Header.DataSize);    
+
+
+    		// Duplication de chaque valeur
+   
+	uint8_t* buffer = (uint8_t*)malloc(grave.Header.DataSize * 2);
+
+	for(int i=0, j=0; i < grave.Header.DataSize; i++, j++)
+	{
+		buffer[j] = source.Data[i];
+
+ 		buffer[++j] = source.Data[i];
+	}
+
+
+            // Mise à l'échelle  
+    
+    int k=0;
+
+	int p = 1;		                                // durée d'un segment en ms
+
+	int seg = grave.Header.Frequence*p/1000;	    //nombre d'échantillon dans un segment de durée p  
+
+	for(int i=0; i < grave.Header.Datasize - seg; i=i+seg)
+	{
+		for(int j=0; j < seg; j++)
+		{
+			grave.Data[j+i] = (buffer[j+i+seg+k*(seg-1)] + buffer[j+i+seg+k*(seg-1)])/2;
+		}
+		k++;
+
+	}  
+
+    fwrite(grave.Data, grave.Header.Datasize, 1, grave.File);
+
+	free(buffer);
+    free(grave.Data);
+
+	fclose(grave.File);
+
 }
 
 void aigue()
@@ -104,36 +261,66 @@ void file_handle()
 	file_size = ftell(source.File);
 	fseek(source.File, 0, SEEK_SET);
 
-    char* buffer;
+    uint8_t* buffer;
     buffer = malloc(file_size);    
 
     fread(buffer, file_size, 1, source.File);
     
-    source.Header.FileTypeBlocID = buffer[i];
+    *((uint32_t*)source.Header.FileTypeBlocID) = *((uint32_t*)(buffer+i));
         i=i+4;
-    source.Header.FileSize = buffer[i];
+    
+    source.Header.FileSize = *((uint32_t*)(buffer+i));
         i=i+4;
-    source.Header.FileFormatID = buffer[i];
+    
+    source.Header.FileFormatID[0] = *(buffer+i);
+        i=i+1;
+    source.Header.FileFormatID[1] = *(buffer+i);
+        i=i+1;
+    source.Header.FileFormatID[2] = *(buffer+i);
+        i=i+1;
+    source.Header.FileFormatID[3] = *(buffer+i);
+        i=i+1;
+
+    source.Header.FormatBlocID[0] = *(buffer+i);
+        i=i+1;    
+    source.Header.FormatBlocID[1] = *(buffer+i);
+        i=i+1; 
+    source.Header.FormatBlocID[2] = *(buffer+i);
+        i=i+1; 
+    source.Header.FormatBlocID[3] = *(buffer+i);
+        i=i+1; 
+    
+    source.Header.BlocSize = *((uint32_t*)(buffer+i));
         i=i+4;
-    source.Header.FormatBlocID = buffer[i];
-        i=i+4;    
-    source.Header.BlocSize = buffer[i];
-        i=i+4;
-    source.Header.AudioFormat = buffer[i];
+
+    source.Header.AudioFormat = *((uint16_t*)(buffer+i));
         i=i+2;
-    source.Header.NbrCanaux = buffer[i];
+
+    source.Header.NbrCanaux = *((uint16_t*)(buffer+i));
         i=i+2;
-    source.Header.Frequence = buffer[i];
+
+    source.Header.Frequence = *((uint32_t*)(buffer+i));
         i=i+4;
-    source.Header.BytePerSec = buffer[i];
+
+    source.Header.BytePerSec = *((uint32_t*)(buffer+i));
         i=i+4;
-    source.Header.BytePerBloc = buffer[i];
+
+    source.Header.BytePerBloc = *((uint16_t*)(buffer+i));
         i=i+2;
-    source.Header.BitsPerSample = buffer[i];
+
+    source.Header.BitsPerSample = *((uint16_t*)(buffer+i));
         i=i+2;
-    source.Header.DataBlocID = buffer[i];
-        i=i+4;
-    source.Header.DataSize = buffer[i]; 
+
+    source.Header.DataBlocID[0] = *(buffer+i);
+        i=i+1;
+    source.Header.DataBlocID[1] = *(buffer+i);
+        i=i+1;
+    source.Header.DataBlocID[2] = *(buffer+i);
+        i=i+1;
+    source.Header.DataBlocID[3] = *(buffer+i);
+        i=i+1;
+
+    source.Header.DataSize = *((uint32_t*)(buffer+i)); 
 
     free(buffer);
 
@@ -141,13 +328,11 @@ void file_handle()
 }
 
 void slice()
-{
-    unsigned long data_size = source.Header.DataSize;
-    
-    source.Data = malloc(data_size);
+{    
+    source.Data = (uint8_t*)malloc(source.Header.DataSize);
 
     fseek(source.File, 44, SEEK_SET);
 
-    fread(source.Data, data_size, 1, source.File);  
+    fread(source.Data, source.Header.DataSize, 1, source.File);  
 }
 
